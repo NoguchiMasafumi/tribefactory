25ce07c9101abd356c9bbd1254cbb65854d119c6
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-03-16 13:32:01 +0900

    Update vue_control.js

--- js/vue_control.js
+++ js/vue_control.js
@@ -136,7 +136,7 @@ var app3 = new Vue({
             explain:'作成中'
           },
           {
-            url:'https://forparents.netlify.app/index6.htm?school=Elementary&year=3&semester=&weak=1',
+            url:'https://forparents.netlify.app/index6.htm?school=Elementary&year=3&semester=&weak=1&status=205981580023441684520784322328773995536304214910232845885484&digit=200',
             text:'for parents',
             img_url:'https://tribefactory.netlify.app/img/forparents.png',
             explain:'漢字問題を出題する際大変だったので作成。中学生まですべて入っている。現在設定は小3。Cookieではなく古の復活の呪文方式に変更したい。2022要項準拠'
