56ccab114b7b142726409abfa338f3d11dbaedc0
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-04-13 19:53:37 +0900

    Update vue_control.js

--- js/vue_control.js
+++ js/vue_control.js
@@ -75,22 +75,6 @@ var app=new Vue({
 
 
 
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })
-
-
 
 
   var value = new Vue({
