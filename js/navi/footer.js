ba57e8fa8c5385374a22dfd656eaf345ff57c6a8
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2025-11-13 19:11:07 +0900

    Update footer.js

--- js/navi/footer.js
+++ js/navi/footer.js
@@ -1,8 +1,8 @@
 const currentYear = new Date().getFullYear();
 const footerHtml = `
-const footerHtml = `
 これはいいってこと？  <-- ✅ 日本語の全角文字（文字列データ）
 <p>&copy; ${currentYear} All Rights Reserved.</p>
 `;
 document.getElementById('footer-container')?.innerHTML = footerHtml;
+
 
