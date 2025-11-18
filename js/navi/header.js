8aa23afa61000a1f4000c9e6cdf07028326ab544
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2025-11-18 20:28:50 +0900

    Update header.js

--- js/navi/header.js
+++ js/navi/header.js
@@ -1,21 +1,16 @@
 const header_contents=`
-
-    <header>
         <div class="header-inner-upper">
             Tribe Factory<img src="https://tribefactory.netlify.app/img/TF20_min.png" class="h24" alt="Tribe Factory" style="margin-left:5px;">
             <!-- navi -->
             <script src="https://tribefactory.netlify.app/js/navi/navi_menu2.js"></script>
         </div>
         <hr />
-    <div class="header-inner-lower">
-        トライブファクトリー
+        <div class="header-inner-lower">
+            トライブファクトリー
 
-    </div>
-    </header>
-
+        </div>
 `;
 const elm_header = document.getElementById('header_container');
 if (elm_header) {
   elm_header.innerHTML = header_contents;
 }
-
